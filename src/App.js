8d15a45e0f83ba43c317a7a29faa2a322425c15c
K Blackmon <keitharuni@msn.com>
2019-10-04 16:14:37 -0400

    fix remaining kitten names

--- src/App.js
+++ src/App.js
@@ -4,23 +4,23 @@ import "./App.css";
 import { SearchBox } from "./hooks/SearchBox";
 
 function App() {
-  const [monsters, setMonsters] = useState([
-    { name: "Keith Blackmon", email: "keith@aol.com", id: 1 },
-    { name: "Kelley Smellbottom", email: "kelley@aol.com", id: 2 },
-    { name: "Kevin Market", email: "kevin@aol.com", id: 3 },
-    { name: "Sam Wells", email: "sam@aol.com", id: 4 },
-    { name: "Sean Theman", email: "sean@aol.com", id: 5 }
+  const [kittens, setKittens] = useState([
+    { name: "Keith Blackmon", email: "keith@fake.com", id: 1 },
+    { name: "Kelley Smellbottom", email: "kelley@fake.com", id: 2 },
+    { name: "Kevin Market", email: "kevin@fake.com", id: 3 },
+    { name: "Sam Wells", email: "sam@fake.com", id: 4 },
+    { name: "Sean Theman", email: "sean@fake.com", id: 5 }
   ]);
   const [searchField, setSearchField] = useState("");
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(res => res.json())
-      .then(users => setMonsters(users));
+      .then(users => setKittens(users));
   });
 
-  const filteredMonsters = monsters.filter(monster =>
-    monster.name.toLowerCase().includes(searchField.toLowerCase())
+  const filteredKittens = kittens.filter(kitten =>
+    kitten.name.toLowerCase().includes(searchField.toLowerCase())
   );
 
   return (
@@ -30,7 +30,7 @@ function App() {
         placeholder='Search Kittens'
         handleChange={e => setSearchField(e.target.value)}
       />
-      <CardList monsters={filteredMonsters} />
+      <CardList kittens={filteredKittens} />
     </div>
   );
 }
